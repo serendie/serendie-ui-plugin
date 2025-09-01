5fde35a1bcb7331c244f05919e8c662491a37288
Masato Nomiyama <nomy@takram.com>
2025-09-01 18:59:20 +0900

    Format codes

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,8 +1,14 @@
+import { Button } from './components/Button'
+
 const { widget } = figma
 const { Frame } = widget
 
 function Widget() {
-  return <Frame width={100} height={100} fill={'#C4C4C4'}></Frame>
+  return (
+    <Frame width={100} height={100} fill={'#C4C4C4'}>
+      <Button onClick={() => console.log('Clicked')}>Button</Button>
+    </Frame>
+  )
 }
 
 widget.register(Widget)
