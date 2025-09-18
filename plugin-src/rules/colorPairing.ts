527777ff858133d76143a9da4d0a52f130230254
Masato Nomiyama <nomy@takram.com>
2025-09-18 12:08:47 +0900

    Improve colorPairing rule

--- plugin-src/rules/colorPairing.ts
+++ plugin-src/rules/colorPairing.ts
@@ -13,6 +13,17 @@ function formatRoles(roles: string | string[] | undefined): string {
   return roles.slice(0, -1).join('、') + `または${roles[roles.length - 1]}`
 }
 
+function isValid(
+  expected: string | string[] | undefined,
+  actual: string
+): boolean {
+  if (!expected) return false
+  if (typeof expected === 'string') {
+    return actual === expected
+  }
+  return expected.includes(actual)
+}
+
 export default function validate(
   textColor: string | null,
   backgroundColor: string | null
@@ -38,21 +49,10 @@ export default function validate(
     return null
   }
 
-  const isValidPairing = (
-    expected: string | string[] | undefined,
-    actual: string
-  ): boolean => {
-    if (!expected) return false
-    if (typeof expected === 'string') {
-      return actual === expected
-    }
-    return expected.includes(actual)
-  }
-
-  const isValidRelationship =
-    isValidPairing(COLOR_PAIRS[textRole], bgRole) ||
-    isValidPairing(COLOR_PAIRS[bgRole], textRole)
-  if (!isValidRelationship) {
+  if (
+    !isValid(COLOR_PAIRS[textRole], bgRole) ||
+    !isValid(COLOR_PAIRS[bgRole], textRole)
+  ) {
     return {
       severity: 'error',
       message: 'テキスト色または背景色が不適切',
