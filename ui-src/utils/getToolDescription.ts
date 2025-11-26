57a44454283911bfac94c70d2fb304325f8c3e66
Masato Nomiyama <nomy@takram.com>
2025-11-26 17:26:35 +0900

    Improve prompt

--- ui-src/utils/getToolDescription.ts
+++ ui-src/utils/getToolDescription.ts
@@ -4,6 +4,16 @@ export default function getToolDescription(toolName: string): string {
       return 'Serendie UIの概要を取得しました'
     case 'get-components':
       return 'Serendie UIのコンポーネント一覧を取得しました'
+    case 'get-symbols':
+      return 'Serendie UIのシンボル一覧を取得しました'
+    case 'get-design-tokens':
+      return 'Serendie Design Tokenの一覧を取得しました'
+    case 'get-component-detail':
+      return 'コンポーネントの詳細情報を取得しました'
+    case 'get-symbol-detail':
+      return 'シンボルの詳細情報を取得しました'
+    case 'get-design-token-detail':
+      return 'デザイントークンの詳細情報を取得しました'
     case 'search-component-docs':
       return 'コンポーネントに関するドキュメントを検索しました'
     case 'search-design-token-docs':
