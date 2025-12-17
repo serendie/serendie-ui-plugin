ef1949c9d46fe5583c69d92d306af7c9d07092b1
Masato Nomiyama <nomy@takram.com>
2025-12-17 18:11:06 +0900

    Shorten frame name in chat message

--- ui-src/components/chat/ChatMessage.tsx
+++ ui-src/components/chat/ChatMessage.tsx
@@ -20,7 +20,6 @@ export default function ChatMessage({
         marginBottom: sd.system.dimension.spacing.large,
         marginRight: role === 'user' ? 0 : 'auto',
         marginLeft: role === 'user' ? 'auto' : 0,
-        width: 'fit-content',
         maxWidth: role === 'user' ? '80%' : '100%',
         display: 'flex',
         flexDirection: 'column',
@@ -36,6 +35,8 @@ export default function ChatMessage({
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'flex-end',
+              maxWidth: '100%',
+              overflow: 'hidden',
             }}
           >
             <img
@@ -44,6 +45,8 @@ export default function ChatMessage({
               style={{
                 backgroundColor: sd.system.color.component.surface,
                 maxHeight: '40vh',
+                maxWidth: '100%',
+                objectFit: 'contain',
               }}
             />
             {imageLabels?.[index] && (
